cbb30098168e7a91b48d61929877e9f82e1c3750
Bego Ripoll <bripollc@gmail.com>
2023-06-20 12:32:37 +0200

    V1

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+
+
+
+def rename_cols(df):
+def select_cols(df):
---
+++ src/clean_data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+def rename_cols(df):
+    """
+    Aquesta funció canvia de nom les columnes del df. Converteix les majúscules en minúscules i posa un guió baix entre paraules en cas de no haver-n'hi.
+    """
+    cols = []
+    for i in range(len(df.columns)):
+        cols.append(df.columns[i].lower().replace(' ', '_'))
+    df.columns = cols
+    return df
+
+
+
+def select_cols(df):
+    """
+    Aquesta funció sel·lecciona les columnes específiques pel df final.
+    """
+    columns = ['id_bcn_2019', 
+                'codi_sector_activitat', 
+                'nom_sector_activitat', 
+               
+                'codi_grup_activitat', 
+                'nom_grup_activitat',  
+               
+                'codi_activitat_2019',
+                'nom_activitat',
+                'nom_local',
+               
+                'latitud',
+                'longitud',
+               
+                'codi_barri',
+                'nom_barri',
+               
+                'codi_districte',
+                'nom_districte']
+
+    df = df[columns]
+
+    return df
